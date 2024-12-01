9094fb689ca580940565a06dc0e2dfb2d435b3c0
Ljiljana Stojicic <ljiljana.stojicic@elfak.rs>
2024-12-01 15:50:58 +0100

    Fja za konverziju

--- board.py
+++ board.py
@@ -43,45 +43,19 @@ class Board:
             print("Rezultat je neresen!")
 
 
-    def matrix_to_position(self, row, col, board_size):
-        if row % 3 != 0:
-            raise ValueError("Pozicija mora biti na liniji sa stubićima.")
-
-        actual_row = row // 3  # Računamo stvarni red
-        if actual_row < board_size:  # Gornji deo i srednji red
-            start_offset = (board_size - actual_row - 1) * 3
-            max_in_row = board_size + actual_row
-        else:  # Donji deo
-            adjusted_row = actual_row - board_size + 1
-            start_offset = (adjusted_row + 1) * 3
-            max_in_row = board_size + (board_size - adjusted_row - 2)
-
-    # Proveravamo validnost kolone
-        actual_col = (col - start_offset) // 6
-        if actual_col < 0 or actual_col >= max_in_row:
-            raise ValueError("Kolona je van opsega za dati red.")
-
-        return f"{chr(65 + actual_row)}{actual_col + 1}"
-
-
-    def position_to_matrix(self, position, board_size):
+    def position_to_matrix(self, position):
         letter = position[0]
-        number = int(position[1:]) - 1
+        number = int(position[1:])
 
         actual_row = ord(letter.upper()) - 65
-        if actual_row < board_size:  # Gornji deo i srednji red
-            start_offset = (board_size - actual_row - 1) * 3
-            max_in_row = board_size + actual_row
-        else:  # Donji deo
-            adjusted_row = actual_row - board_size + 1
-            start_offset = (adjusted_row + 1) * 3
-            max_in_row = board_size + (board_size - adjusted_row - 2)
-
-        if number < 0 or number >= max_in_row:
-            raise ValueError("Pozicija je van opsega za dati red.")
 
         row = actual_row * 3
-        col = start_offset + number * 6
+        if(actual_row == self.boardSize - 1):
+            col = 2 + 6 * (number - 1)
+        if(actual_row < self.boardSize - 1):
+            col = 3 * (abs(self.boardSize - 1 - actual_row)) + 2 + 6 * (number - 1)
+        else:
+            col = 3 * (abs(-self.boardSize + 1 + actual_row)) + 2 + 6 * (number - 1)
         return row, col
 
 
--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 from board import Board
 from structures import Player
 
-board = Board(True, 4, Player.O)
+board = Board(True, 5, Player.O)
 board.initialize()
 board.print()
 print(board.halfPoints)
-print(board.position_to_matrix('A1', 4))
+print(board.position_to_matrix('B1'))
